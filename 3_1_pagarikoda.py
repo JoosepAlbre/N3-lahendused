3e302dcbb9c037c3f65c8bc8ee71470856280095
JoosepAlbre <56920425+JoosepAlbre@users.noreply.github.com>
2023-05-03 01:50:44 +0300

    Update 3_1_pagarikoda.py

--- 3_1_pagarikoda.py
+++ 3_1_pagarikoda.py
@@ -2,20 +2,20 @@
 import math
 
 def koogi_hind(kook, x):
-  if kook == "Napoleoni kook":
-      return round(0.08 * x * x, 2)
-  elif kook == "šokolaadikook":
-      return round(0.05 * math.pi * x * x, 2)
-  elif kook == "ploomikook":
-      return round(0.04 * math.pi * x * x, 2)
-  else:
-      return -1
+    if kook == "Napoleoni kook":
+        return round(0.08 * x * x, 2)
+    elif kook == "šokolaadikook":
+        return round(0.05 * math.pi * x * x, 2)
+    elif kook == "ploomikook":
+        return round(0.04 * math.pi * x * x, 2)
+    else:
+        return -1
 
-  nimi = input("Sisesta koogi nimi: ")
-  mõõt = float(input("Sisesta koogi mõõt: "))
+nimi = input("Sisesta koogi nimi: ")
+mõõt = float(input("Sisesta koogi mõõt: "))
+hind = koogi_hind(nimi, mõõt)
 
-  hind = koogi_hind(nimi, mõõt)
-  if hind < 0:
-      print("Sellist kooki pagarikoda ei valmista.")
-  else:
-      print("Selle koogi hind on " + str(hind) + " eurot.")
+if hind < 0:
+    print("Sellist kooki pagarikoda ei valmista.")
+else:
+    print("Selle koogi hind on " + str(hind) + " eurot.")
